646356f17026f55123f656f08f8fc21318ceb22f
Ahmad Raza <ahmadraza_@outlook.com>
2020-11-13 05:22:31 +0500

    Scraped url, title, and abstract of the papers.

--- bot_.py
+++ bot_.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
 		for x in range(1, argc):
 			query.append(sys.argv[x])
 
-		#query_str = " ".join(query) + ":pdf"
 		query_str = " ".join(query)
 		
 		chrome_options = webdriver.ChromeOptions()
@@ -29,30 +28,28 @@ if __name__ == "__main__":
 		search_bar.send_keys(query_str)
 		search_bar.submit()
 
-		time.sleep(10) ## your wait was not working
+		time.sleep(10)
 
 		main_div = driver.find_element(By.CLASS_NAME, 'result-page')
 		
-		a_tags = main_div.find_elements_by_css_selector('div > a')
+		a_tags = main_div.find_elements_by_css_selector('div.cl-paper-row.serp-papers__paper-row > a')
 		title_spans = main_div.find_elements_by_css_selector('div > a > div > span')
-		abstract_expand = main_div.find_elements_by_css_selector('span.more.mod-clickable')
-		#abstract_spans = main_div.find_elements_by_css_selector('div.cl-paper-abstract > span')
-
+		#abstract_expand = main_div.find_elements_by_css_selector('div.cl-paper-row.serp-papers__paper-row > div > div > span.more.mod-clickable')
+		abstract_expand = main_div.find_elements(By.CLASS_NAME, 'more.mod-clickable')
+		
 		urls = [a.get_attribute('href') for a in a_tags]
 		titles = [title.text for title in title_spans]
 		
 		time.sleep(20)
-
+		
 		timeout = 10
 		for clickable in abstract_expand:
-			#clickable.send_keys(Keys.RETURN)
-			#clickable.send_keys("\n")
-			#WebDriverWait(driver, timeout).until(EC.presence_of_element_located(clickable))
 			WebDriverWait(driver, timeout).until(EC.visibility_of(clickable))
 			clickable.click()
-			time.sleep(15)
-
-		abstract_spans = main_div.find_elements_by_css_selector('div.cl-paper-abstract > span')
+			time.sleep(10)
+		
+		#abstract_spans = main_div.find_elements_by_css_selector('div.cl-paper-abstract > span')
+		abstract_spans = main_div.find_elements_by_css_selector('div.tldr-abstract-replacement.text-truncator')
 		abstracts = [abstract.text for abstract in abstract_spans]
 
 		list_ = []
