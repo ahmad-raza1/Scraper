848ecbd8de8d4cbeccb88735708baafb1fcb95b7
Ahmad Raza <ahmadraza_@outlook.com>
2021-04-02 20:04:37 +0500

    Resolved issues of missing abstracts of some results."

--- bot_.py
+++ bot_.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import constants
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -19,7 +20,7 @@ if __name__ == "__main__":
 		query_str = " ".join(query)
 		
 		chrome_options = webdriver.ChromeOptions()
-		#chrome_options.add_argument('--headless')
+		chrome_options.add_argument('--headless')
 		driver = webdriver.Chrome(options=chrome_options)
 		driver.maximize_window()
 
@@ -28,45 +29,69 @@ if __name__ == "__main__":
 		search_bar.send_keys(query_str)
 		search_bar.submit()
 
-		time.sleep(10)
+		while True:
+			try:
+				main_div = driver.find_element(By.CLASS_NAME, 'result-page')
+				break
+			except Exception as e:
+				print("DOM loading...\n")
 
-		main_div = driver.find_element(By.CLASS_NAME, 'result-page')
-		
-		a_tags = main_div.find_elements_by_css_selector('div.cl-paper-row.serp-papers__paper-row > a')
-		title_spans = main_div.find_elements_by_css_selector('div > a > div > span')
-		#abstract_expand = main_div.find_elements_by_css_selector('div.cl-paper-row.serp-papers__paper-row > div > div > span.more.mod-clickable')
-		abstract_expand = main_div.find_elements(By.CLASS_NAME, 'more.mod-clickable')
-		
-		urls = [a.get_attribute('href') for a in a_tags]
-		titles = [title.text for title in title_spans]
-		
-		time.sleep(20)
-		
-		timeout = 10
-		for clickable in abstract_expand:
-			WebDriverWait(driver, timeout).until(EC.visibility_of(clickable))
-			clickable.click()
-			time.sleep(10)
-		
-		#abstract_spans = main_div.find_elements_by_css_selector('div.cl-paper-abstract > span')
+		splits = main_div.find_elements_by_css_selector('.cl-paper-row.serp-papers__paper-row.paper-row-normal')
+
+		abst_getable = []
+		abstract_expand_btns = []
+
+		for x in range(len(splits)): 
+		    try:
+		        abstract_div = splits[x].find_element_by_css_selector('div.tldr-abstract-replacement')
+		        abstract_expand_btns.append(abstract_div.find_element_by_css_selector('span.more.mod-clickable'))
+		        abst_getable.append(True)
+
+		    except:
+		        abst_getable.append(False)
+
+		print(abst_getable)
+
+		for x in range(len(abstract_expand_btns)):
+		    WebDriverWait(driver, constants.TIMEOUT).until(EC.visibility_of(abstract_expand_btns[x]))
+		    driver.execute_script("arguments[0].click();", abstract_expand_btns[x])
+
+		url_hrefs = main_div.find_elements_by_css_selector('div.cl-paper-row.serp-papers__paper-row > a')
+		title_spans =  main_div.find_elements_by_css_selector('div > a > div > span')
+
+		urls = [a.get_attribute('href') for a in url_hrefs]
+		titles = [title.text.encode('utf8') for title in title_spans]
+
 		abstract_spans = main_div.find_elements_by_css_selector('div.tldr-abstract-replacement.text-truncator')
-		abstracts = [abstract.text for abstract in abstract_spans]
+		abstracts = [abstract.text.encode('utf8') for abstract in abstract_spans]
 
 		list_ = []
-		for url, title, abstract in zip(urls, titles, abstracts):
-			list_.append(
-				{
-					"url" : url,
-					"title" : title,
-					"abstract" : abstract
-				}
-			);
+		i = 0
+
+		for x in range(len(splits)):
+		    if abst_getable[x] == True:
+		            list_.append(
+		            {
+		            "url" : urls[x],
+		            "title" : titles[x],
+		            "abstract" : abstracts[i]
+		            }
+		        );
+		            i = i + 1
+		    else:
+		        list_.append(
+		            {
+		            "url" : urls[x],
+		            "title" : titles[x],
+		            "abstract" : ""
+		            }
+		        );
 
 		print(*list_, sep = "\n\n")
 
 		with open('listfile.txt', 'w') as filehandle:
-			for listitem in list_:
-				filehandle.write('%s\n\n' % listitem)
+		    for listitem in list_:
+		        filehandle.write('%s\n\n' % listitem)
 
 		driver.close()
 
