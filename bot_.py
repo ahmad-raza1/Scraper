1f2e38798f11d2b4c1edb67e948a7e65edb31048
Ahmad Raza <ahmadraza_@outlook.com>
2021-06-27 00:34:12 +0500

    Improvisation

--- bot_.py
+++ bot_.py
@@ -31,7 +31,6 @@ if __name__ == "__main__":
 			try:
 				pagination_div = driver.find_element(By.CLASS_NAME, 'cl-pager')
 				break
-
 			except Exception as e:
 				print("DOM loading...\n")
 
@@ -39,7 +38,7 @@ if __name__ == "__main__":
 		#print(len(results), "\n")
 		#print(len(pages))
 
-		flag = [];
+		flag = []; # array for keeping track of results that have open-access PDFs
 		urls = []; pdf_urls = []; titles = []; abstracts = []
 
 		for x in range(len(pages)):
@@ -48,11 +47,10 @@ if __name__ == "__main__":
 					main_div = driver.find_element(By.CLASS_NAME, 'result-page')
 					splits = main_div.find_elements_by_css_selector('.cl-paper-row.serp-papers__paper-row.paper-row-normal')
 					break
-
 				except Exception as e:
 					pass
 
-			time.sleep(5)
+			#time.sleep(5)
 			splits = main_div.find_elements_by_css_selector('.cl-paper-row.serp-papers__paper-row.paper-row-normal')
 			
 			temp = []
@@ -63,33 +61,26 @@ if __name__ == "__main__":
 			empty_flag = True
 
 			for y in range(len(splits)):
-				try:
-					hello = splits[y].find_element_by_css_selector('span.cl-button__label')
-
-				except Exception as e:
-					pass
-
 				pdf_btn = splits[y].find_element_by_css_selector('span.cl-button__label')
 
 				if pdf_btn.text == "View PDF on arXiv":
 					empty_flag = False
 
-					# pdfs
+					# PDFs
 					pdf_url = splits[y].find_element_by_css_selector('a.flex-row.cl-paper-view-paper')
 					pdf_url_hrefs.append(pdf_url)
 
 					# Let's now pick abstracts!!!
 					try:
 						abstract_div = splits[y].find_element_by_css_selector('div.tldr-abstract-replacement')
-					
 					except:	# try second option
 						abstract_div = splits[y].find_element_by_css_selector('div.cl-paper-abstract')
 
 					# abstracts
 					abstract_spans.append(abstract_div)
 					abstract_expand_btns.append(abstract_div.find_element_by_css_selector('span.more.mod-clickable'))
-
-					# url and title
+					
+					# urls and titles
 					url_hrefs.append(splits[y].find_element_by_css_selector('div.cl-paper-row.serp-papers__paper-row > a'))
 					title_spans.append(splits[y].find_element_by_css_selector('div.cl-paper-title'))
 
@@ -99,14 +90,21 @@ if __name__ == "__main__":
 				else:
 					temp.append(False)
 
-			# if there are pdfs on the page
+			# if there are PDFs on the page
 			if not empty_flag:
 				# click abstract expand-links
-				for z in range(len(abstract_expand_btns)):
-					WebDriverWait(driver, constants.TIMEOUT).until(EC.visibility_of(abstract_expand_btns[z]))
-					driver.execute_script("arguments[0].click();", abstract_expand_btns[z])
+				for btn in abstract_expand_btns:
+					WebDriverWait(driver, constants.TIMEOUT).until(EC.visibility_of(btn))
+					
+					# wait until button is not clickable
+					while True:
+						try:
+							driver.execute_script("arguments[0].click();", btn)
+							break
+						except Exception as e:
+							pass
 
-				# save text in the lists
+				# save text in separate lists
 				for url, pdf_url, title, abstract in zip(
 					url_hrefs, pdf_url_hrefs, title_spans, abstract_spans):
 
@@ -116,19 +114,36 @@ if __name__ == "__main__":
 					abstracts.append(abstract.text)
 
 			flag.append(temp)
-			print("Page {}:".format(x + 1), flag[x], "\n")
+			print("\nPage {}:".format(x + 1), flag[x], "\n")
+			print("----------------------------------\n")
 
 			if x < len(pages) - 1:
 				WebDriverWait(driver, constants.TIMEOUT).until(EC.visibility_of(pages[x + 1]))
-				driver.execute_script("arguments[0].click();", pages[x + 1])
+
+				while True:
+					try:
+						driver.execute_script("arguments[0].click();", pages[x + 1])
+						break
+					except Exception as e:
+						pass
+				
+				# halt a little bit to let the next page load properly
+				time.sleep(3)
 
 		# save the final result
-		list_ = {}
+		list_ = []
 
-		list_["urls"] = urls
-		list_["pdf_urls"] = pdf_urls
-		list_["titles"] = titles
-		list_["abstracts"] = abstracts
+		for url, pdf_url, title, abstract in zip(
+			urls, pdf_urls, titles, abstracts):
+			
+			list_.append(
+				{
+					"url": url,
+					"pdf_url": pdf_url,
+					"title": title,
+					"abstract": abstract
+				}
+			);
 
 		print("\n\n----------------------------------\n\n")
 		print(json.dumps(list_, sort_keys=False, indent=4))
